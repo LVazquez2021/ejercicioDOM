19832aa164a043d53ab6d767d54fefbd1b51fd10
Luis Vazquez <luisvazquezcontadorpublico@gmail.com>
2021-04-27 22:24:57 -0300

    exercise manipulando DOM

--- exercise/index.js
+++ exercise/index.js
@@ -1,13 +1,25 @@
 const btnAgregarCaja = document.getElementById('btnAddBox');
-const inputValue = document.getElementById('entrada')
+const input = document.getElementById('entrada')
 const divContenedor = document.getElementById("contenedor");
+const input2 = document.getElementById('entrada2');
+
+
+function limpiar() {
+    input.value = '';
+    input2.value = '';
+}
 
 btnAgregarCaja.addEventListener('click', function() {
     const nuevaCaja = document.createElement('div');
     nuevaCaja.classList.add('caja');
+    const nuevaImg = document.createElement('img');
+    nuevaImg.classList.add('image')
+    nuevaImg.setAttribute('src', input2.value);
     const nuevaParrafo = document.createElement('p');
-    nuevaParrafo.textContent = inputValue.value;
-    nuevaParrafo.classList.add('parrafo')
-    nuevaCaja.appendChild(nuevaParrafo)
+    nuevaParrafo.textContent = input.value;
+    nuevaParrafo.classList.add('parrafo');
+    nuevaCaja.appendChild(nuevaImg);
+    nuevaCaja.appendChild(nuevaParrafo);
     divContenedor.appendChild(nuevaCaja)
+    limpiar();
 });
